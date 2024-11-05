c16277fbe7440d660350e21a3c6e1dc87d9b9e4a
Igor Cristian <100322776+IgorCristian@users.noreply.github.com>
2024-11-04 21:59:34 -0300

    Add files via upload

--- Python/Aula 1/exercicio04.py
+++ Python/Aula 1/exercicio04.py
@@ -1,2 +1,10 @@
 # Ex04. Faça um programa que peça 6 numeros, e armazene-os em duas listas: "pares" e "impares".
 # Depois mostre-as na tela
+
+numeros = []
+pares = []
+impares = []
+
+for i in range(1,7):
+    numero = int(input(f'Digite o {i}º número da lista: '))
+    numeros.append(numero)
