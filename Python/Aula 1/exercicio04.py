5e016babcf36e97ed35d83c39b08bd04a7da2192
Igor Cristian <100322776+IgorCristian@users.noreply.github.com>
2024-11-10 22:21:43 -0300

    Add files via upload

--- Python/Aula 1/exercicio04.py
+++ Python/Aula 1/exercicio04.py
@@ -1,10 +1,14 @@
 # Ex04. Faça um programa que peça 6 numeros, e armazene-os em duas listas: "pares" e "impares".
 # Depois mostre-as na tela
 
-numeros = []
 pares = []
 impares = []
 
 for i in range(1,7):
     numero = int(input(f'Digite o {i}º número da lista: '))
-    numeros.append(numero)
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
+print(f"Estes são os números pares: {pares}")
+print(f"Estes são os números ímpares: {impares}")
