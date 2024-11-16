e6021142026a213b6a434806b9e2e93cb6f97c9b
Igor Cristian <100322776+IgorCristian@users.noreply.github.com>
2024-11-16 14:34:37 -0300

    Delete Python directory (Old archives)

--- Python/Aula 1/exercicio02.py
+++
@@ -1,8 +0,0 @@
-# Ex02. Faça um programa que peça 5 numeros e armazene-os em uma lista
-lista_de_numeros = []
-
-for i in range(1,6):
-    numero = input(f'Digite o {i}º número da lista: ')
-    lista_de_numeros.append(numero)
-
-print(lista_de_numeros)
--- Python/Aula 2/exercicio03.py
+++
@@ -1,5 +0,0 @@
-#Ex03. Dado o dicionário abaixo, remova o item com a chave "idade" (utilizando a função pop()) e exiba o dicionário atualizado.
-
-"""python
-estoque = {"banana": 10, "laranja": 5, "uva": 8}
-"""
