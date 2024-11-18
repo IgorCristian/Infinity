bd04520e1e81949e7c92971619bc3a33112d7e7d
Igor Cristian <100322776+IgorCristian@users.noreply.github.com>
2024-11-18 20:21:01 -0300

    Update exercicio03.py
    Corrigindo questão do exercício

--- Módulo 2 - Python/Aula 2/exercicio03.py
+++ Módulo 2 - Python/Aula 2/exercicio03.py
@@ -1,4 +1,4 @@
-#Ex03. Dado o dicionário abaixo, remova o item com a chave "idade" (utilizando a função pop()) e exiba o dicionário atualizado.
+#Ex03. Dado o dicionário abaixo, remova o item com a chave "laranja" (utilizando a função pop()) e exiba o dicionário atualizado.
 
 """python
 estoque = {"banana": 10, "laranja": 5, "uva": 8}
