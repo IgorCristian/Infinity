c2b3f31b3207898768273dc1c77a0bb332a75227
Igor Cristian <100322776+IgorCristian@users.noreply.github.com>
2024-12-14 15:37:46 -0300

    Prova da aula 1

---
+++ Módulo 2 - Python/Aula 1/PROVA.py
@@ -0,0 +1,12 @@
+# [PYIA-A01] Crie uma lista contendo seis frutas de sua escolha.
+# Depois de ter a lista pronta, converta essa lista em uma tupla.
+# Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+# Cria a lista de frutas
+lista_frutas = ['maçã', 'pêra', 'banana', 'laranja', 'limão', 'abacate']
+
+# Converte a lista em tupla
+tupla_frutas = tuple(lista_frutas)
+
+# Exibe o conteúdo da tupla
+print(tupla_frutas)
